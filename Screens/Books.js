f2df1bc3cb85898a4e85c43c141962887ff4e1ac
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-07-24 01:19:24 +0200

    books page ok

--- Screens/Books.js
+++ Screens/Books.js
@@ -1,15 +1,93 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Image, ScrollView, StyleSheet, StatusBar, SafeAreaView } from 'react-native'
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Ionicons } from '@expo/vector-icons';
 
-function Books(){
+
+function Books(props){
+
+    const [isLoading, setIsLoging] = useState(true);
+    const [dataSource, setDataSource] = useState(null);
+    
+    const displayBooks = async () => {
+        const data = await fetch('https://api-dev.lelivrescolaire.fr/graphQL')
+        const response = await data.json();
+        console.log('response', response);
+    }
+    displayBooks();
 
     return(
-    <View style={{ flex: 1, backgroundColor:'#e67e22'}}>   
-
+        <SafeAreaView style={styles.container}>
+            <ScrollView
+                style={styles.scrollview}
+                contentContainerStyle={styles.contentContainerStyle}> 
+                <Card>
+                    {/* <Card.Divider/> */}
+                    <Card.Image source={require('../images/avatar1.png')}>
+                    </Card.Image>
+                    {/* <Text style={{marginBottom: 10}}>
+                        The idea with React Native Elements 
+                    </Text> */}
+                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Button
+                        title='Ouvrir le livre'
+                        icon={<Icon name='arrow-forward' color='#ffffff' />}
+                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                        />
+                </Card>
+                <Card>
+                    {/* <Card.Divider/> */}
+                    <Card.Image source={require('../images/avatar1.png')}>
+                    </Card.Image>
+                    {/* <Text style={{marginBottom: 10}}>
+                        The idea with React Native Elements 
+                    </Text> */}
+                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Button
+                        title='Ouvrir le livre'
+                        icon={<Icon name='arrow-forward' color='#ffffff' />}
+                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                        />
+                </Card>
+                <Card>
+                    {/* <Card.Divider/> */}
+                    <Card.Image source={require('../images/avatar1.png')}>
+                    </Card.Image>
+                    {/* <Text style={{marginBottom: 10}}>
+                        The idea with React Native Elements 
+                    </Text> */}
+                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Button
+                        title='Ouvrir le livre'
+                        icon={<Icon name='arrow-forward' color='#ffffff' />}
+                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                        />
+                </Card>
 
     
-    </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 export default Books;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollview: {
+        flex: 1,
+        marginTop: 80,
+        
+    },
+    contentContainerStyle: {
+        display: 'flex',
+        flexDirection: 'row', 
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start', 
+        flexWrap: 'wrap' 
+    },
+    
+});
