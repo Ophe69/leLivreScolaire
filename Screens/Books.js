ad497b18a7609735726a02369ed071a9cdc1dde7
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-07-24 15:56:23 +0200

    get valid datas from API

--- Screens/Books.js
+++ Screens/Books.js
@@ -6,6 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 
 function Books(props){
 
+    const [booksList, setBooksList] = useState([]);
     const [isLoading, setIsLoging] = useState(true);
     const [dataSource, setDataSource] = useState(null);
     
@@ -39,58 +40,54 @@ function Books(props){
             })
         
     });
+    
     const response = await data.json();
-    console.log('ce que donne la response', response);
+    console.log('la response', response.data.viewer.books.hits[1].displayTitle);
+    setBooksList(response.data.viewer.books.hits)
+    console.log('******** BOOKSLIST******', booksList);
+    console.log(booksList.length);
+
+
+
+    //setBooksList({response})
+    //console.log('voici la booklist', booksList)
         }
         displayBooks()
-    })
+    }, [])
+
+
+    
+    
+
+    
     return(
         <SafeAreaView style={styles.container}>
             <ScrollView
                 style={styles.scrollview}
                 contentContainerStyle={styles.contentContainerStyle}> 
-                <Card>
-                    {/* <Card.Divider/> */}
-                    <Card.Image source={require('../images/avatar1.png')}>
-                    </Card.Image>
-                    {/* <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements 
-                    </Text> */}
-                    <Card.Title>HELLO WORLD</Card.Title>
-                    <Button
-                        title='Ouvrir le livre'
-                        icon={<Icon name='arrow-forward' color='#ffffff' />}
-                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                        />
-                </Card>
-                <Card>
-                    {/* <Card.Divider/> */}
-                    <Card.Image source={require('../images/avatar1.png')}>
-                    </Card.Image>
-                    {/* <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements 
-                    </Text> */}
-                    <Card.Title>HELLO WORLD</Card.Title>
-                    <Button
-                        title='Ouvrir le livre'
-                        icon={<Icon name='arrow-forward' color='#ffffff' />}
-                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                        />
-                </Card>
-                <Card>
-                    {/* <Card.Divider/> */}
-                    <Card.Image source={require('../images/avatar1.png')}>
-                    </Card.Image>
-                    {/* <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements 
-                    </Text> */}
-                    <Card.Title>HELLO WORLD</Card.Title>
-                    <Button
-                        title='Ouvrir le livre'
-                        icon={<Icon name='arrow-forward' color='#ffffff' />}
-                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                        />
-                </Card>
+
+                <View style={styles.cards}>
+                    {booksList.map((book,i)=>(
+                    <View key={i} style={{display:'flex',justifyContent:'center'}}>
+                        <Card>
+                            {/* <Card.Divider/> */}
+                            <Card.Image source={require('../images/avatar1.png')}>
+                            </Card.Image>
+                            {/* <Text style={{marginBottom: 10}}>
+                                The idea with React Native Elements 
+                            </Text> */}
+                            <Card.Title>HELLO WORLD</Card.Title>
+                            <Button
+                                title='Ouvrir le livre'
+                                icon={<Icon name='arrow-forward' color='#ffffff' />}
+                                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                                />
+                        </Card>
+                    </View>
+                ))}
+                </View>
+                
+                
 
     
             </ScrollView>
@@ -111,6 +108,14 @@ const styles = StyleSheet.create({
         
     },
     contentContainerStyle: {
+        display: 'flex',
+        flexDirection: 'row', 
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start', 
+        flexWrap: 'wrap' 
+    },
+    cards: {
+        flex: 1,
         display: 'flex',
         flexDirection: 'row', 
         justifyContent: 'flex-start',
