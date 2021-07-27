98f0d21d6f73a48371706727ed3a919fcd4426b3
Jonathan <jonathan@siruplab.com>
2021-07-27 11:18:00 +0200

    Review

--- App.js
+++ App.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Ionicons } from "@expo/vector-icons"
 
-import Books from './Screens/Books';
-import Lessons from './Screens/Lessons';
-import Chapters from './Screens/Chapters';
+import Books from "./Screens/Books"
+import Lessons from "./Screens/Lessons"
+import Chapters from "./Screens/Chapters"
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
-export default function App() {
-  return (
-    <NavigationContainer>
+const App = () => (
+  <NavigationContainer>
     <Tab.Navigator
-
-    screenOptions={({ route }) => ({
+      screenOptions={({ route }) => ({
         tabBarIcon: ({ color }) => {
-          let iconName;
+          let iconName
 
-
-          if (route.name === 'Books') {
-            iconName = 'book';
-          } else if (route.name === 'Chapters') {
-            iconName = 'bookmarks';
-          } else if (route.name === 'Lessons') {
-            iconName = 'document';
+          if (route.name === "Books") {
+            iconName = "book"
+          } else if (route.name === "Chapters") {
+            iconName = "bookmarks"
+          } else if (route.name === "Lessons") {
+            iconName = "document"
           }
 
-          return <Ionicons name={iconName} size={25} color={color} />;
+          return <Ionicons name={iconName} size={25} color={color} />
         },
       })}
       tabBarOptions={{
-        activeTintColor: '#0984e3',
-      inactiveTintColor: '#dfe6e9',
+        activeTintColor: "#0984e3",
+        inactiveTintColor: "#dfe6e9",
       }}
-      >
+    >
+      {/*TODO: Here, another type of navigation could have been more appropriate. Do you which one?*/}
       <Tab.Screen name="Books" component={Books} />
       <Tab.Screen name="Chapters" component={Chapters} />
       <Tab.Screen name="Lessons" component={Lessons} />
     </Tab.Navigator>
   </NavigationContainer>
-  );
-}
+)
 
+export default App
+
+// TODO: Except with a comment saying why, it's never a good idea to leave commented code in a project
 /* const styles = StyleSheet.create({
   container: {
     flex: 1,
--- Screens/Chapters.js
+++ Screens/Chapters.js
@@ -1,169 +1,143 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, Image, ScrollView, StyleSheet, StatusBar, SafeAreaView, FlatList } from 'react-native';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import React, { useState, useEffect, useCallback } from "react"
+import { View, Text, Image, ScrollView, StyleSheet, StatusBar, SafeAreaView } from "react-native"
+import { Card } from "react-native-elements"
 
-function Chapters(props){
+const Chapters = () => {
+  const [chaptersList, setChaptersList] = useState([])
 
-    const [chaptersList, setChaptersList] = useState([]);
-    const [chapterTitle, setChapterTitle] = useState('');
-    const [chapterImage, setChapterImage] = useState('');
-    const [chapterID, setChapterID] = useState('');
+  const getChapters = useCallback(async () => {
+    const data = await fetch("https://api-dev.lelivrescolaire.fr/graphQL", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `query chapters($bookId:Int){viewer{chapters(bookIds:[$bookId]){hits{id title url valid}}}}`,
+        variables: {
+          bookId: 1339497,
+        },
+      }),
+    })
 
-    const navigation = props.navigation
+    const response = await data.json()
+    setChaptersList(response?.data?.viewer?.chapters?.hits)
+    console.log("chapterlist:", chaptersList)
+  }, [])
 
-    
+  useEffect(() => {
+    // noinspection JSIgnoredPromiseFromCall
+    getChapters()
+  }, [])
 
-
-    useEffect (() => {
-        const getChapters = async () => { 
-            const data = await fetch('https://api-dev.lelivrescolaire.fr/graphQL', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                        query: `query chapters($bookId:Int){viewer{chapters(bookIds:[$bookId]){hits{id title url valid}}}}`,
-                        variables: {
-                            bookId: 1339497
-                        }
-            })
-    });
-    
-    const response = await data.json();
-    setChaptersList(response.data.viewer.chapters.hits);
-    console.log('chapterlist:', chaptersList)
-    
-        }
-        getChapters()
-    }, [])
-
-
-    return(
+  return (
     <SafeAreaView style={styles.container}>
-            <ScrollView
-                style={styles.scrollview}
-                contentContainerStyle={styles.contentContainerStyle}> 
-                
-                {/* <View style={styles.bookImage}>
+      <ScrollView style={styles.scrollview} contentContainerStyle={styles.contentContainerStyle}>
+        {/* <View style={styles.bookImage}>
                     <Image
                         source={require('../images/avatar1.png')}
                         size={150}
                     />
                     <Text>Titre du Livre</Text>
                 </View>     */}
-                
-    {chaptersList.map((chapter, i) => {
-        return (
-            <View key={i} style={styles.chapterPage}>
 
-    
-                <Card
-                    opacity={chapter.valid ? 1 : 0.4}
-                    
-                >
-                <Card.Title style={{fontSize: 25, color: '#27b4d7'}}>Chapitre {i}</Card.Title>
-                <Card.Divider/>
-                    <View style={styles.chapterCard}>
-                    <Image
-                        style={styles.cardImage}
-                        resizeMode="cover"
-                        source={{ uri: chapter.url }}
-                    />
-                    <Text style={styles.cardText}>{chapter.title}</Text>
-                    </View>
-                </Card>
+        {
+            // TODO: Using a map for an unknown number of elements to show is a bad idea. Here's why: https://stackoverflow.com/a/48104178. How would you adapt this code?
+            chaptersList.map((chapter, i) => {
+          return (
+            <View key={i} style={styles.chapterPage}>
+              <Card opacity={chapter.valid ? 1 : 0.4}>
+                <Card.Title style={{ fontSize: 25, color: "#27b4d7" }}>Chapitre {i}</Card.Title>
+                <Card.Divider />
+                <View style={styles.chapterCard}>
+                  <Image
+                    style={styles.cardImage}
+                    resizeMode="cover"
+                    source={{ uri: chapter.url }}
+                  />
+                  <Text style={styles.cardText}>{chapter.title}</Text>
+                </View>
+              </Card>
             </View>
-            );
-        })
-    }
-            </ScrollView>
-        </SafeAreaView>
-    );
+          )
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-
-export default Chapters;
+export default Chapters
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: StatusBar.currentHeight,
-        display:'flex',
-    
-    },
-    scrollview: {
-        flex: 1,
-        marginTop: 20,
-        display: 'flex',
-        flexDirection:'column',
-        
-    },
-    activityIndicator: {
-        position: 'absolute',
-        left: 20,
-        right: 20,
-        top: 20,
-        bottom: 20,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    display: "flex",
+  },
+  scrollview: {
+    flex: 1,
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "column",
+  },
+  activityIndicator: {
+    position: "absolute",
+    left: 20,
+    right: 20,
+    top: 20,
+    bottom: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    contentContainerStyle: {
-        display: 'flex',
-        flexDirection: 'row', 
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start', 
-        flexWrap: 'wrap' 
-    },
-    chapterPage: {
-        backgroundColor: '#27b4d7',
+  contentContainerStyle: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+  },
+  chapterPage: {
+    backgroundColor: "#27b4d7",
+  },
+  bookImage: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    },
-    bookImage: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  cards: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+  },
+  title: {
+    display: "flex",
+    color: "black",
+    marginTop: 10,
+  },
+  chapterCard: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
 
-    cards: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row', 
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start', 
-        flexWrap: 'wrap' 
-    },
-    title: {
-        display: 'flex',
-        color: 'black',
-        marginTop: 10,
+  cardImage: {
+    height: 100,
+    width: 100,
+    padding: 5,
+  },
 
-    },
-    chapterCard: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        
-
-    },
-
-    cardImage: {
-        height: 100,
-        width: 100,
-        padding: 5,
-
-    },
-
-    cardText: {
-        height: 100,
-        width: 200,
-        marginTop: 10,
-        marginRight: 20,
-        marginLeft: 20,
-        fontSize: 15,
-        paddingTop: 20,
-        
-    },
-    
-});
+  cardText: {
+    height: 100,
+    width: 200,
+    marginTop: 10,
+    marginRight: 20,
+    marginLeft: 20,
+    fontSize: 15,
+    paddingTop: 20,
+  },
+})
